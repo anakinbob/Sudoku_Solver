5a72cabf90816d9f283cbe0642fe2e980ad3a50d
anakinbob <aannkkiittbb@gmail.com>
2016-09-17 18:52:29 -0400

    added skeleton for row,column, and box extractors

--- src/main/java/Sudoku.java
+++ src/main/java/Sudoku.java
@@ -1,24 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toSet;
+
 public class Sudoku {
     private int _puzzleSize;
+    private int[][] _puzzle;
 
     public Sudoku(int puzzleSize) {
         _puzzleSize = puzzleSize;
     }
 
     public void runSudoku() {
-        Console.print(generateSudokuString(createSudokuPuzzle(_puzzleSize),_puzzleSize));
-
+        _puzzle = createSudokuPuzzle(_puzzleSize);
+        Console.print(generateSudokuString(_puzzle,_puzzleSize));
     }
 
-    private short[][] createSudokuPuzzle(int puzzleSize) {
-        Console.print("Enter each number from top left going right in order\n");
-        short[][] puzzle = new short[puzzleSize][];
+    private int[][] createSudokuPuzzle(int puzzleSize) {
+        Console.print("Enter each number from top left going right in order, 0 for unknown number\n");
+        int[][] puzzle = new int[puzzleSize][];
         for(int i = 0; i < puzzleSize; i++) {
-            short[] row = new short[puzzleSize];
+            int[] row = new int[puzzleSize];
             for(int j = 0; j < puzzleSize; j++) {
                 try {
-                    short num = Short.parseShort(Console.read());
-                    if(num > puzzleSize) {
+
+                    int num = Integer.parseInt(Console.read());
+                    if(num > puzzleSize || num < 0) {
                         throw new NumberFormatException();
                     }
                     row[j] = num;
@@ -32,15 +41,48 @@ public class Sudoku {
         return puzzle;
     }
 
-    private String generateSudokuString(short[][] puzzle, int puzzleSize) {
+    private String generateSudokuString(int[][] puzzle, int puzzleSize) {
         StringBuilder puzzleBuilder = new StringBuilder("");
         for(int i = 0; i < puzzleSize; i++) {
             for(int j = 0; j < puzzleSize; j++) {
-                puzzleBuilder.append(puzzle[i][j]);
+                if(puzzle[i][j] == 0) {
+                    puzzleBuilder.append("_");
+                } else {
+                    puzzleBuilder.append(puzzle[i][j]);
+                }
                 puzzleBuilder.append(" ");
             }
             puzzleBuilder.append("\n");
         }
         return puzzleBuilder.toString();
+    }
+
+    private int[][] solve(int[][] puzzle) {
+        return null;
+    }
+
+    private Set<Integer> extractRow(int rowNum) {
+        Set<Integer> row = new HashSet<>();
+        //IntStream intStream = Arrays.stream(_puzzle[rowNum]).boxed().collect(Collectors.toSet());
+        //List<Integer> a = Arrays.asList(_puzzle[1]);
+        return null;
+    }
+
+    private Set<Integer> extractBox(int box) {
+        return null;
+    }
+
+    private Set<Integer> extractColumn(int column) {
+        return null;
+    }
+    
+    private Set<Integer> generatePossibleNumbers(Set<Integer> numsInBox, Set<Integer> numsInRow, Set<Integer> numsInColumn) {
+        Set<Integer> possibleNumList = new HashSet<Integer>();
+        for(int i = 1; i <= _puzzleSize; i++) {
+            if(!numsInBox.contains(i) && !numsInRow.contains(i) && !numsInColumn.contains(i)) {
+                possibleNumList.add(i);
+            }
+        }
+        return possibleNumList;
     }
 }
